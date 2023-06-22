670308a3d45e7dcad8917e07c661cfc8dc757d07
Luke Calderon <luke.a.calderon@gmail.com>
2023-06-22 16:20:06 -0500

    investment project start

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your buying price per share:");
+        double buyingPrice = scan.nextDouble();
+        int day = 1;
+        double closingPrice = 0.1;
+
+        while (true) {
+            System.out.println("Enter the closing price for day "
+                    + day + " (any negative value to leave: ");
+            closingPrice = scan.nextDouble();
+            if (closingPrice < 0.0) break;
+            double earnings = closingPrice - buyingPrice;
+            if (earnings > 0){
+                System.out.println("After day " + day + " you have earned "
+                        + earnings + " per share.");
+            }
+            else if (earnings < 0){
+                System.out.println("After day " + day + " you have lost "
+                        + (-earnings) + " per share.");
+            }
+            else {
+                System.out.println("After day " + day + " you have broken even.");
+            }
+        }
     }
 }
